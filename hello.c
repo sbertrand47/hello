97177adc8ff7cabdfd6bb2e0db19a552bdcc1884
Bertrand SIBOMANA <bsibomana2@huskers.unl.edu>
2021-08-27 17:02:31 +0000

    Initial Commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Bertrand Sibomana
+ *
+ * Date: 2021/08/27
+ *
+ * HACK 01
+ *
+ *A program to print out my name
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Bertrand Sibomana \n");
+
+  return 0;
+}
